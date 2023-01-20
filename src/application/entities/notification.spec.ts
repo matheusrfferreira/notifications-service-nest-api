3da724060510c95c9081a6b3549f77641338c8cf
matheusrfferreira <mrff@tuta.io>
2023-01-20 18:25:36 -0300

    initial commit

---
+++ src/application/entities/notification.spec.ts
@@ -0,0 +1,13 @@
+import { Notification } from './notification';
+import { Content } from './content';
+
+describe('Notification', () => {
+  it('should be able to create a notification', () => {
+    const notification = new Notification({
+      content: new Content('new invite!'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+    expect(notification).toBeTruthy();
+  });
+});
